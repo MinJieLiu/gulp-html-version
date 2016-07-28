6d8f0f307aa4fd25bbdc795bf4364d1974b4fa49
LiuMY <LiuMY@m.zhinanmao.com>
2016-07-28 10:09:57 +0800

    comment

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var assign = require('object-assign');
 var gutil = require('gulp-util');
 var pkg = require(__dirname + '/package.json');
 
-// 插件名称
+// plugin name
 const PLUGIN_NAME = 'gulp-html-version';
 
-// 默认参数
+// default parameter
 var defaults = {
     paramName: 'v',
     paramType: 'version',
@@ -15,7 +15,7 @@ var defaults = {
 };
 
 /**
- * 生成 shortId
+ * Short unique id generator
  */
 var ShortId = function() {
     var lastTime;
@@ -33,11 +33,11 @@ var ShortId = function() {
 
 function gulpHtmlVersion(options) {
 
-    // 合并参数
+    // merge
     var opts = assign(defaults, options);
     var shortId = new ShortId();
 
-    // 选择参数类型
+    // switch a parameter
     switch (opts.paramType) {
         case 'version':
             opts.version = pkg.version;
@@ -50,11 +50,10 @@ function gulpHtmlVersion(options) {
             break;
     }
 
-    // 初始化匹配正则
+    // init regexp
     var suffix = opts.suffix.join('|');
     var regex = new RegExp('(\\s[\\w-]+=".+)(\\.' + suffix + ')(\\?[^&]+(?:&[^&]+)*)?(")', 'ig');
 
-    // 创建一个让每个文件通过的 stream 通道
     var stream = through.obj(function(file, enc, cb) {
 
         if (file.isNull()) {
@@ -68,10 +67,10 @@ function gulpHtmlVersion(options) {
         }
 
         var contents = file.contents.toString();
-        // 替换
+        // replace
         contents = contents.replace(regex, function(match, $1, $2, $3, $4) {
             var version;
-            // 追加参数
+            // append parameter
             if ($3 != undefined) {
                 version = $3 + '&' + opts.paramName + '=' + opts.version;
             } else {
