0a9e2bda2acd73c9bd1da1838035447f31d5ae12
MinJieLiu <liumingyi_1@163.com>
2018-07-11 17:25:12 +0800

    fixed #1

--- index.js
+++ index.js
@@ -9,84 +9,81 @@ const PLUGIN_NAME = 'gulp-html-version';
 
 // default parameter
 var defaults = {
-    paramName: 'v',
-    paramType: 'version',
-    suffix: ['css', 'js']
+  paramName: 'v',
+  paramType: 'version',
+  suffix: ['css', 'js'],
 };
 
 /**
  * Short unique id generator
  */
-var ShortId = function() {
-    var lastTime;
+var ShortId = function () {
+  var lastTime;
 
-    this.next = function() {
-        var d = new Date();
-        var thisTime = (d.getTime() - Date.UTC(d.getUTCFullYear(), 0, 1)) * 1000;
-        while (lastTime >= thisTime) {
-            thisTime++;
-        }
-        lastTime = thisTime;
-        return thisTime.toString(16);
-    };
+  this.next = function () {
+    var d = new Date();
+    var thisTime = (d.getTime() - Date.UTC(d.getUTCFullYear(), 0, 1)) * 1000;
+    while (lastTime >= thisTime) {
+      thisTime++;
+    }
+    lastTime = thisTime;
+    return thisTime.toString(16);
+  };
 };
 
 function gulpHtmlVersion(options) {
 
-    // merge
-    var opts = assign(defaults, options);
-    var shortId = new ShortId();
+  // merge
+  var opts = assign(defaults, options);
+  var shortId = new ShortId();
 
-    // switch a parameter
-    switch (opts.paramType) {
-        case 'version':
-            opts.version = pkg.version;
-            break;
-        case 'guid':
-            opts.version = shortId.next();
-            break;
-        case 'timestamp':
-            opts.version = Date.now();
-            break;
+  // switch a parameter
+  switch (opts.paramType) {
+    case 'version':
+      opts.version = pkg.version;
+      break;
+    case 'guid':
+      opts.version = shortId.next();
+      break;
+    case 'timestamp':
+      opts.version = Date.now();
+      break;
+  }
+
+  var regex = new RegExp('(\\s[\\w-]+=".+)(\\.' + opts.suffix.join('|') + ')(\\?[^&"]+(?:&[^&"]+)*)?(")', 'ig');
+
+  var stream = through.obj(function (file, enc, cb) {
+
+    if (file.isNull()) {
+      this.push(file);
+      return cb();
     }
 
-    // init regexp
-    var suffix = opts.suffix.join('|');
-    var regex = new RegExp('(\\s[\\w-]+=".+)(\\.' + suffix + ')(\\?[^&]+(?:&[^&]+)*)?(")', 'ig');
+    if (file.isStream()) {
+      this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+      return cb();
+    }
 
-    var stream = through.obj(function(file, enc, cb) {
-
-        if (file.isNull()) {
-            this.push(file);
-            return cb();
-        }
-
-        if (file.isStream()) {
-            this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
-            return cb();
-        }
-
-        var contents = file.contents.toString();
-        // replace
-        contents = contents.replace(regex, function(match, $1, $2, $3, $4) {
-            var version;
-            // append parameter
-            if ($3 != undefined) {
-                version = $3 + '&' + opts.paramName + '=' + opts.version;
-            } else {
-                version = '?' + opts.paramName + '=' + opts.version;
-            }
-            return $1 + $2 + version + $4;
-        });
-
-        file.contents = new Buffer(contents);
-
-        this.push(file);
-
-        cb();
+    var contents = file.contents.toString();
+    // replace
+    contents = contents.replace(regex, function (match, $1, $2, $3, $4) {
+      var version;
+      // append parameter
+      if ($3 != undefined) {
+        version = $3 + '&' + opts.paramName + '=' + opts.version;
+      } else {
+        version = '?' + opts.paramName + '=' + opts.version;
+      }
+      return $1 + $2 + version + $4;
     });
 
-    return stream;
+    file.contents = new Buffer(contents);
+    this.push(file);
+
+    cb();
+  });
+
+  return stream;
 }
 
 module.exports = gulpHtmlVersion;
