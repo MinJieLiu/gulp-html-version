3954afd70595a9ede00983fe106bc996e08bdbe6
MinJieLiu <liumingyi_1@163.com>
2018-07-11 17:22:24 +0800

    fixed #1

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 gulpHtmlVersion = require('../index');
 
-gulp.task('default', function() {
-    return gulp.src('./*.html')
-        .pipe(gulpHtmlVersion())
-        .pipe(gulp.dest('./build/'));
+gulp.task('default', function () {
+  return gulp.src('./*.html')
+    .pipe(gulpHtmlVersion({
+      paramName: 'version',
+      suffix: ['css', 'js', 'png'],
+    }))
+    .pipe(gulp.dest('./build/'));
 });
